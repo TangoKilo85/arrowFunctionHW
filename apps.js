b34d35a5601d6b61bd7cad1038e60df7d6567454
Tyler Wellman <TangoKilo1985@gmail.com>
2022-07-18 08:33:03 -0400

    gitz

---
+++ apps.js
@@ -0,0 +1,43 @@
+let x = 10;
+
+let y = 2;
+
+function getAdd (x,y) {
+    var add = (x + y);
+    console.log(add);
+
+}
+
+getAdd(x,y);
+
+function getSubtract (x,y) {
+    var subtract = (x - y);
+    console.log(subtract);
+
+}
+
+getSubtract(x,y);
+
+function getMultiply (x,y) {
+    var multiply = (x * y);
+    console.log(multiply);
+
+}
+
+getMultiply(x,y);
+
+function getDivide (x,y) {
+    var divide = (x / y);
+    console.log(divide);
+
+}
+
+getDivide(x,y);
+
+let a = (x, y) => x + y;
+
+let b = (x, y) => x - y;
+
+let c = (x, y) => x * y;
+
+let d = (x, y) => x / y;
